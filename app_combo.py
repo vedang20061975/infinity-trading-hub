ce3dcc23543cc4f46578938dfe514c5e73845eb0
vedang20061975 <bharatparmar0703@gmail.com>
2026-07-21 19:00:25 +0530

    Update BASE_URL for Google script webhook

--- app_combo.py
+++ app_combo.py
@@ -5,7 +5,7 @@ import pandas as pd
 # =====================================================================
 # 🎯 CONFIGURATION LOCK (તમારી નવી ફાઇનલ ગુગલ સ્ક્રિપ્ટ વેબહૂક લિંક)
 # =====================================================================
-BASE_URL = "https://script.google.com/macros/s/AKfycbwFMoyWgptzxBB3eSZEZUoc_s5Zsrkg7mJB_1wzshKasHkJgejQxMYL35PignszDBHpkA/exec"
+BASE_URL = "https://script.google.com/macros/s/AKfycbwCX61eQsn1_jckPUASEvJ7853BORVeeyBS_HF22XrZj3ZElYvARO4veQJ-cXJqbyiyaA/exec"
 MASTER_KEY = "BharatSir@Infinity"
 
 # Page Settings
